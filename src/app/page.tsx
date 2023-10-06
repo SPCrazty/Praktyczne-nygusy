9f20cda0f0aa50a5603aa3542b97de1141c1918e
SPCrazty <cypek.pisarek@gmail.com>
2023-10-06 17:39:06 +0200

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from 'next/image'
 import './globals.css'
-import Navbar from './navbar'
+import NavBar from './navbar'
 
 
 export default function Home() {
   return (
    <main className='w-screen '>
-    <Navbar/>
+    <NavBar/>
    </main>
   )
 }
